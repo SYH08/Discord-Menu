bd96e0208bac78cf478c82dff9369b4d3e46a4e5
SYH <85495636+SYH08@users.noreply.github.com>
2024-04-06 07:06:28 +0900

    re-add

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Client(intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+@bot.command()
+async def hello(ctx):
+    await ctx.send('ㅂㅅ')
+
+bot.run("TKN")
