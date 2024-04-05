27080b2cfd6549a84d20d5d6283b96340363d03e
SYH <85495636+SYH08@users.noreply.github.com>
2024-04-06 08:48:18 +0900

    Todo

--- functions.py
+++ functions.py
@@ -9,3 +9,8 @@ class get:
                 self.data = json.load(JSONData)
                 print("JSON Parsed")
                 return(self.data["Token"])
+                
+#내가보기에 이거 어짜피 한번 콜 해줘야되니까
+#걍 어....겟 있어야되나?
+#다른 러이브러리들 참고해서 고쳐야되긴 함
+#뭐가 문젠진 모름
