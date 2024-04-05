de045b18a4df55a4fe30685c1758b7fa07cc0631
SYH <85495636+SYH08@users.noreply.github.com>
2024-04-06 08:53:03 +0900

    Update functions.py
    Minimalize

--- functions.py
+++ functions.py
@@ -1,14 +1,13 @@
 import json
 
 class get:
-    class getData:
-        def __init__(self):
-            self.data = ""
-        def Token(self):
-            with open("private.json", "r") as JSONData:
-                self.data = json.load(JSONData)
-                print("JSON Parsed")
-                return(self.data["Token"])
+    def __init__(self):
+        self.data = ""
+    def Token(self):
+        with open("private.json", "r") as JSONData:
+            self.data = json.load(JSONData)
+            print("JSON Parsed")
+            return(self.data["Token"])
                 
 #내가보기에 이거 어짜피 한번 콜 해줘야되니까
 #걍 어....겟 있어야되나?
