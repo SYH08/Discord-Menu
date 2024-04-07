92c6b353ef5cbda47faee07f0dc5b366a9088d27
SYH <85495636+SYH08@users.noreply.github.com>
2024-04-07 22:07:39 +0900

    Date

--- pharser.py
+++ pharser.py
@@ -1,15 +1,35 @@
 import csv
+from datetime import datetime
+
+date = datetime.today().day
+weekday = 4 #datetime.today().weekday()
+weeknum = 0
 
 f = open("April.csv", "r")
 reader = csv.reader(f)
-
-# for row in reader:
-#     print(row)
+sliceNum = [1, 10, 19, -1]
 
 data = list(reader)
-print(data)
-print(data[6])
+print(len(data))
 del data[:16]
-del data[20:]
+del data[29:]
 print(data)
-print(data)
+
+data = list(filter(None, data))
+print()
+print(data[10][0])
+
+l = 1
+menu = []
+print()
+print(data[sliceNum[1]][4])
+print()
+
+print(menu)
+
+print(date)
+
+if weekday > 4:
+    print("오늘은 주말입니다.")
+else:
+    print(data[sliceNum[weeknum]][weekday-1])
